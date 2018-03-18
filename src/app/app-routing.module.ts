22108e1de82c4ba1eab34c1e99f40331c5f67b51
Evans Chikarakara <bangophinx@gmail.com>
2018-03-18 01:38:36 +0000

    Started Project, added components and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'client/add', component: AddClientComponent},
+  {path: 'client/edit/:id', component: EditClientComponent},
+  {path: 'client/:id', component: ClientDetailsComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', component: NotFoundComponent},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
+export const routingComponents =
+  [
+    DashboardComponent, AddClientComponent, ClientDetailsComponent, ClientsComponent, EditClientComponent, LoginComponent, NavbarComponent, NotFoundComponent, RegisterComponent, SettingsComponent,SidebarComponent
+  ]
+
